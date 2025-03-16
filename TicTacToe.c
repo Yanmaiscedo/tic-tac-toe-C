7d398c13d9da44cf374964d12605ca78a2897fb5
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 18:47:07 -0300

    Feat: Draw function

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char matrix[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char player = 'X';
+int n;
+
+void Draw() {
+    system("cls"); // No Linux, use: printf("\033[H\033[J");
+    printf("!!!!El Roego De la Abuelita!!!!\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("      %c ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
 
     return 0;
